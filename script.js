09d28725527b8f11c1ce6681fa769fb4d07bc272
fogueteg <foguetegedalprinha@gmail.com>
2026-04-15 18:56:04 +0000

    ta ai k

---
+++ script.js
@@ -0,0 +1,69 @@
+/*
+const filmesFavoritos = ["Divertidamente", "Matrix", "O Senhor das Coisas"];
+
+filmesFavoritos.push("Os carinha");
+
+console.log(filmesFavoritos);
+console.log("Quantidade de filmes:", filmesFavoritos.length);
+*/
+/*
+const cores = ["vermei", "verde", "azul"]
+console.log(cores)
+console.log(cores[1]);
+*/
+/*
+const usuario = {
+  nome: "Pedro",
+  idade: "19",
+  estudante: false
+};
+
+usuario.idade = usuario.idade.replace("19", "21");
+
+console.log(usuario);
+*/
+/*
+let numerodomal = 15.2564234
+resultado = numerodomal.toFixed(2) 
+console.log(resultado)
+*/
+/*
+String = "eu sou minusculo kk"
+nah = String.toUpperCase(0);
+console.log(nah)
+*/
+/*
+const lista = ["batata", "arroz", "coisas"]
+console.log(lista.includes("batata"))
+*/
+/*
+String = " eu tenho espaço no inicio e no final "
+console.log(String.trim())
+*/
+/*
+const sala = [
+
+     { nome: "ronaldo",
+       nota: 2
+    },
+     { nome: "bernardo",
+       nota: 10
+    },
+]
+
+console.log(sala)
+*/
+/*
+const filmesFavoritos = ["Divertidamente", "Matrix", "O Senhor das Coisas"];
+
+filmesFavoritos.pop()
+
+console.log(filmesFavoritos);
+console.log("Quantidade de filmes:", filmesFavoritos.length);
+*/
+/*
+const frase = "JavaScript é legal";
+const resultado = frase.split(" ");
+
+console.log(resultado);
+*/
